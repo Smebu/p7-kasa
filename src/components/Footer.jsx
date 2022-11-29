c7a18d79e99cbf3c3de9ff52995dafda91ab71fe
Semih <semih.tasdemir@outlook.fr>
2022-11-29 13:19:51 +0100

    end of the project
    fin du responsive et du projet Kasa

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="footer">
-      <img src="./logo-white.png" alt="Logo white" className="footer_img" />
+      <img src={process.env.PUBLIC_URL + "/logo-white.png"} alt="Logo white" className="footer_img" />
       <p className="footer_text">© 2020 Kasa. All rights reserved</p>
     </footer>
   );
